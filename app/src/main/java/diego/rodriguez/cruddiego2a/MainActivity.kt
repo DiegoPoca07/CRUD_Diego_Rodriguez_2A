c719d6a444c25584609298be354f228543b3396f
Diego Rodriguez <20230602@ricaldone.edu.sv>
2024-05-02 09:46:13 -0600

    Subiendo  mi proyecto al repositorio

--- app/src/main/java/diego/rodriguez/cruddiego2a/MainActivity.kt
+++ app/src/main/java/diego/rodriguez/cruddiego2a/MainActivity.kt
@@ -1,6 +1,8 @@
 package diego.rodriguez.cruddiego2a
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //1- Mandar a llamar todos los elementos de la pantalla
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtPrecio = findViewById<EditText>(R.id.txtPrecio)
+        val btnAgregar = findViewById<Button>(R.id.btnButton)
+
+        //2- Programar el boton
+        btnAgregar.setOnClickListener {
+            
+        }
+
     }
 }
