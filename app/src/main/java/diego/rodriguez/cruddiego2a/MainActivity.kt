d0e00640128cf86382b3c9abcaeab0d74e8c7909
Diego Rodriguez <20230602@ricaldone.edu.sv>
2024-05-02 10:54:35 -0600

    Subi la otra parte de codigo

--- app/src/main/java/diego/rodriguez/cruddiego2a/MainActivity.kt
+++ app/src/main/java/diego/rodriguez/cruddiego2a/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +26,25 @@ class MainActivity : AppCompatActivity() {
         //1- Mandar a llamar todos los elementos de la pantalla
         val txtNombre = findViewById<EditText>(R.id.txtNombre)
         val txtPrecio = findViewById<EditText>(R.id.txtPrecio)
+        val txtCantidad = findViewById<EditText>(R.id.txtCantidad)
         val btnAgregar = findViewById<Button>(R.id.btnButton)
 
         //2- Programar el boton
         btnAgregar.setOnClickListener {
-            
+            GlobalScope.launch(Dispatchers.IO){
+
+                //Guardar datos
+                //1- Creo un objeto de la clase conexion
+                val claseConexion = ClaseConexion().cadenaConexion()
+
+                //2- Creo una variable que contenga un PreparedStatement
+                val addProducto = claseConexion?.prepareStatement("insert into tbProductos(nombreProducto, precio, cantidad) values(?, ?, ?)")!!
+                addProducto.setString(1, txtNombre.text.toString())
+                addProducto.setInt(2, txtPrecio.text.toString().toInt())
+                addProducto.setInt(3, txtCantidad.text.toString().toInt())
+                addProducto.executeUpdate()
+
+            }
         }
 
     }
